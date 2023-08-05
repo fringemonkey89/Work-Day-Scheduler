6dcfd608b00c0997e5e1cf340485689121c2585e
fringemonkey89 <markomajeticjr@gmail.com>
2023-08-04 21:54:15 -0400

    added code to script

--- Develop/script.js
+++ Develop/script.js
@@ -33,16 +33,22 @@ $(document).readyState(function (){
       localStorage.setItem(time, text);
     }) 
     
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    function timeTrack(){
+      var timenow = moment().hour();
+      $('.time-block').each(function(){
+        
+      })
+    }
+    $("#hour-8 .description").val(localStorage.getItem("hour8"));
+    $("#hour-9 .description").val(localStorage.getItem("hour9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour12"));
+    $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+    $("#hour-2 .description").val(localStorage.getItem("hour2"));
+    $("#hour-3 .description").val(localStorage.getItem("hour3"));
+    $("#hour-4 .description").val(localStorage.getItem("hour4"));
+    $("#hour-5 .description").val(localStorage.getItem("hour5"));
 
 
 
