01217aa05559774ab093960e2740317d0b779d4a
fringemonkey89 <markomajeticjr@gmail.com>
2023-08-07 18:29:54 -0400

    added comments to javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,14 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-//$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  //display date and todays date
   var todayDate = moment().format('dddd, MMM Do YYYY');
   $("#currentDay").text(todayDate);
 
 
 $(document).ready(function () {
+  //adding click listener to saveBtn
     $(".saveBtn").on("click", function(){
 
+    //gets values from the description
       var text = $(this).siblings(".description").val();
       var time = $(this).parent().attr("id");
 
@@ -34,11 +16,13 @@ $(document).ready(function () {
     }) 
 
       function timeTrack() {
+      //get current time
       var timenow = moment().hour();
-      
+      // looping through all the time block elements
       $(".time-block").each(function (){
       var blocktime = parseInt($(this).attr("id").split("hour")[1]);
       
+     //checks the time and class to change background colors
       if (blocktime < timenow){
         $(this).removeClass("future");
         $(this).removeClass("present");
@@ -63,7 +47,7 @@ $(document).ready(function () {
     $("#hour-10 .description").val(localStorage.getItem("hour10"));
     $("#hour-11 .description").val(localStorage.getItem("hour11"));
     $("#hour-12 .description").val(localStorage.getItem("hour12"));
-    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-13 .description").val(localStorage.getItem("hour13"));
     $("#hour-14 .description").val(localStorage.getItem("hour14"));
     $("#hour-15 .description").val(localStorage.getItem("hour15"));
     $("#hour-16 .description").val(localStorage.getItem("hour16"));
